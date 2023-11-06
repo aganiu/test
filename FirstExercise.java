4fa6d22e88a39278bad0ecf2330e4827854f8abf
aganiu <148196352+aganiu@users.noreply.github.com>
2023-11-06 11:23:42 +0200

    Add files via upload

---
+++ FirstExercise.java
@@ -0,0 +1,24 @@
+import java.util.scanner
+
+public class FirstExercise{
+	public static void main(String[] args){
+
+	Scanner scan = new Scanner(System.in);
+
+	System.out.println("Player 1 please enter the card number 1-10: ")
+	int chosenNumber = scan.nextInt();
+
+	System.out.println("Player 1 enter the suit of your card, type c for Clubs, s for Spades, d for Diamonds and h for hearts. ");
+	int chosenSuit = scan.next().charAt(0);
+
+	System.out.println("Player 2 please enter the card number 1-10: ");
+	int chosenNumber = scan.nextInt();
+
+	System.out.println("Player 2 please enter the suit of your card, type c for Clubs, s for Spades, d for Diamonds and h for hearts. ");
+
+	if(chosenSuit == "s" || chosenSuit != "S" &&)
+
+	}
+
+}
+
